165f24913c2d60a3c7ee45c3d90e700393853038
Junhe Chen <contact@junhechen.com>
2026-08-03 21:15:44 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] dp = new int[n + 1];
+        // base case
+        dp[n] = 0; // we got here 
+        dp[n - 1] = 1; // you can only climb up one to get there
+        dp[n - 2] = 2;
+        for(int i = n - 3; i >= 0; i --){
+            dp[i] = dp[i + 1] + dp[i + 2];
+        }
+        return dp[0];
+    }
+}
